95b355d92a3ad050407858af5ddaecf1ac6892ee
Leung190299 <leung01628381807@gmail.com>
2023-01-17 10:17:34 +0700

    Disable privacy

--- src/Settings.php
+++ src/Settings.php
@@ -43,6 +43,7 @@ class Settings {
 							$this->checkbox( 'no_embeds', __( 'Disable embeds, e.g. prevent others from embedding your site and vise-versa', 'falcon' ) );
 							$this->checkbox( 'no_revisions', __( 'Disable revisions', 'falcon' ) );
 							$this->checkbox( 'no_self_pings', __( 'Disable self pings', 'falcon' ) );
+							$this->checkbox( 'no_privacy', __( 'Disable privacy', 'falcon' ) );
 							?>
 						</div>
 						<div class="tab-pane hidden" id="tab-header">
@@ -72,6 +73,7 @@ class Settings {
 							$this->checkbox( 'no_dashboard_widgets', __( 'Remove default dashboard widgets', 'falcon' ) );
 							$this->checkbox( 'no_wp_logo', __( 'Remove WordPress logo in the admin bar', 'falcon' ) );
 							$this->checkbox( 'no_admin_email_confirm', __( 'Remove admin email confirmation', 'falcon' ) );
+							$this->checkbox( 'no_application_passwords', __( 'Remove admin passwords', 'falcon' ) );
 							?>
 						</div>
 
