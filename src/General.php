e78db4b96a121a1145866ac9257dd59adaac338f
Anh Tran <anhtnt@elightup.com>
2023-08-12 15:15:22 +0700

    Move code of disable oembed into the plugin

--- src/General.php
+++ src/General.php
@@ -67,7 +67,20 @@ class General extends Base {
 	}
 
 	public function no_embeds() {
-		require FALCON_DIR . '/vendor/disable-embeds/disable-embeds.php';
+		// Remove the REST API endpoint.
+		remove_action( 'rest_api_init', 'wp_oembed_register_route' );
+
+		// Turn off oEmbed auto discovery.
+		add_filter( 'embed_oembed_discover', '__return_false' );
+
+		// Don't filter oEmbed results.
+		remove_filter( 'oembed_dataparse', 'wp_filter_oembed_result' );
+
+		// Remove oEmbed discovery links.
+		remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
+
+		// Remove oEmbed JavaScript from the front-end and back-end.
+		remove_action( 'wp_head', 'wp_oembed_add_host_js' );
 	}
 
 	public function no_revisions() {
