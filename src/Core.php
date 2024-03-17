9a8f9f1ba9ce5d485a6ec8041d6e934807002753
Anh Tran <anhtnt@elightup.com>
2024-03-17 17:17:46 +0700

    Redirect to settings page after activation

--- src/Core.php
+++ src/Core.php
@@ -3,8 +3,33 @@ namespace Falcon;
 
 class Core {
 	public function __construct() {
+		add_action( 'activated_plugin', [ $this, 'redirect' ], 10, 2 );
 		add_filter( 'plugin_action_links_falcon/falcon.php', [ $this, 'add_plugin_action_links' ] );
 		add_filter( 'plugin_row_meta', [ $this, 'add_plugin_meta_links' ], 10, 2 );
+	}
+
+	public function redirect( $plugin, $network_wide = false ): void {
+		$is_cli    = 'cli' === php_sapi_name();
+		$is_plugin = 'falcon/falcon.php' === $plugin;
+
+		$action           = isset( $_POST['action'] ) ? sanitize_text_field( wp_unslash( $_POST['action'] ) ) : ''; // phpcs:ignore WordPress.Security.NonceVerification
+		$checked          = isset( $_POST['checked'] ) && is_array( $_POST['checked'] ) ? count( $_POST['checked'] ) : 0; // phpcs:ignore WordPress.Security.NonceVerification
+		$is_bulk_activate = $action === 'activate-selected' && $checked > 1;
+
+		if ( ! $is_plugin || $network_wide || $is_cli || $is_bulk_activate || $this->is_bundled() ) {
+			return;
+		}
+		wp_safe_redirect( admin_url( 'options-general.php?page=falcon' ) );
+		die;
+	}
+
+	private function is_bundled(): bool {
+		foreach ( $_REQUEST as $key => $value ) { // phpcs:ignore WordPress.Security.NonceVerification
+			if ( str_contains( $key, 'tgmpa' ) || ( is_string( $value ) && str_contains( $value, 'tgmpa' ) ) ) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public function add_plugin_action_links( array $links ): array {
