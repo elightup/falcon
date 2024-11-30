992b019bed276b4d714475ed59b37a6b06411847
Anh Tran <anhtnt@elightup.com>
2024-11-30 17:22:37 +0700

    No Gutenberg: remove support for post types

--- src/General.php
+++ src/General.php
@@ -24,6 +24,7 @@ class General extends Base {
 	public function no_gutenberg() {
 		// Disable Gutenberg on the back end.
 		add_filter( 'use_block_editor_for_post', '__return_false' );
+		add_filter( 'use_block_editor_for_post_type', '__return_false' );
 
 		// Disable Gutenberg for widgets.
 		add_filter( 'use_widgets_block_editor', '__return_false' );
